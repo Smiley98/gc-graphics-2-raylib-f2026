96712930a033f5a511abd4bd04540eb09046fa79
Smiley98 <smileysofficialcinema@gmail.com>
2026-09-24 09:17:35 -0400

    Added more motion and raylib functions

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -8,25 +8,36 @@ int main()
     InitAudioDevice();
     SetTargetFPS(60);
 
-    float pos_x = 0.0f;
-    float pos_y = 400.0f;
-    float speed = 250.0f;
+    Vector2 pos1 = { 0.0f, GetScreenHeight() * 0.5f };
+    Vector2 pos2 = { GetScreenWidth() * 0.5f, GetScreenHeight() * 0.5f };
 
     while (!WindowShouldClose())
     {
         float total_time = GetTime();
         float frame_time = GetFrameTime();
 
-        pos_x += speed * frame_time;
-        if (pos_x + 20.0f >= GetScreenWidth())
-            pos_x = 0.0f;
+        pos1.x += 250.0f * frame_time;
+        if (pos1.x + 20.0f >= GetScreenWidth())
+            pos1.x = 0.0f;
 
-        pos_y = sinf(total_time * 8.0f) * 100.0f + GetScreenHeight() * 0.5f;
+        pos1.y = sinf(total_time * 8.0f) * 100.0f + GetScreenHeight() * 0.5f;
+
+        // Rotate at 100 degrees per second in a radius of 100, then translate by half the screen
+        Matrix translation_1 = MatrixTranslate(100.0f, 0.0f, 0.0f);
+        Matrix rotation = MatrixRotateZ(total_time * 100.0f * DEG2RAD);
+        Matrix translation_2 = MatrixTranslate(GetScreenWidth() * 0.5f, GetScreenHeight() * 0.5f, 0.0f);
+
+        Vector2 pos_2_rotated = Vector2UnitX * translation_1 * rotation * translation_2;
+        DrawCircleV(pos_2_rotated, 20.0f, PURPLE);
+
+        DrawLineV(pos1, pos_2_rotated, LIME);
+        DrawLineEx({ GetScreenWidth() * 0.5f, GetScreenHeight() * 0.5f }, GetMousePosition(), 4.0f, ORANGE);
 
         BeginDrawing();
         ClearBackground(WHITE);
-
-        DrawCircle(pos_x, pos_y, 20.0f, BLUE);
+        DrawFPS(10, 10);
+        DrawText("Hello raylib", 660, 10, 20, DARKBLUE);
+        DrawCircleV(pos1, 20.0f, BLUE);
         DrawCircleV(GetMousePosition(), 20.0f, RED);
 
         EndDrawing();
